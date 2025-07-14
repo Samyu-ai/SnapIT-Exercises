0bf1f2b859a4b2aa35ff5e761ead03426cf7ba71
Samyu-ai <samyu.a25@gmail.com>
2025-07-14 16:05:50 -0500

    Create stringslab.js

---
+++ stringslab.js
@@ -0,0 +1,9 @@
+//Task 1
+const myString = " This statement has empty spaces ";
+console.log(myString.includes(""));
+console.log(myString.at(1));
+console.log(myString.trim());
+console.log(myString.toLowerCase());
+
+//Task 2
+console.log("Jack and Jill went up the hill to fetch a pail of water.\nJack fell down and broke his crown and Jill came tumbling after.");
