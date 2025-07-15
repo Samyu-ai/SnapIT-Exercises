f9e63c3706d651241b8c3dbfebe6213fdbeeb4dd
Samyu-ai <samyu.a25@gmail.com>
2025-07-14 23:46:31 -0500

    Create numberslab.js

---
+++ numberslab.js
@@ -0,0 +1,23 @@
+const valueA = 34_038
+console.log(valueA);
+
+const valueB = 45_838.8734
+console.log(valueB);
+
+const valueC = 0b0110_0010_1011
+console.log(valueC);
+
+const valueD = 0x1_74C0;
+console.log(valueD);
+
+const valueE = -895_612;
+console.log(valueE);
+
+const binary = 0b11101
+console.log(binary);
+
+const hex = 0x1D;
+console.log(hex);
+
+const octal = 0o35;
+console.log(octal);
